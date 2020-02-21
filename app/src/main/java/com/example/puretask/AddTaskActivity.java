2a522c69b447e460b3601c1e165ae4492db0ea85
Mason Ashment <masonashment@gmail.com>
2020-02-20 23:08:16 -0500

    add task activity started

---
+++ app/src/main/java/com/example/puretask/AddTaskActivity.java
@@ -0,0 +1,41 @@
+package com.example.puretask;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    private EditText nameEditText,descriptionEditText;
+    private Button saveButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nameEditText = findViewById(R.id.nameEditText);
+        descriptionEditText = findViewById(R.id.descriptionEditText);
+        saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/com/example/puretask/MainActivity.java
+++ app/src/main/java/com/example/puretask/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.puretask;
 
+import android.content.Intent;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -49,10 +50,14 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                createNewTask();
             }
         });
+    }
+
+    public void createNewTask() {
+        Intent addIntent = new Intent(this,AddTaskActivity.class);
+        startActivity(addIntent);
     }
 
     @Override
